c5634639820fcc6b23b5e8b6fcf538c1fbcc2f17
ChristianMoreira98 <159092067+ChristianMoreira98@users.noreply.github.com>
2026-07-20 23:25:10 -0300

    Faculdade EROS

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+//Seleciona o body da página HTML
+const body = document.querySelector('body');
+
+//Seleciona o ícone dentro do botão de tema
+const iconeTema = document.querySelector('#icone-tema');
+
+//Função para alternar o tema
+function alternarTema(){
+    //Adiciona ou remove a class dark
+    body.classList.toggle('dark');
+};
+
+
+
+function processarInscricao(){
+const nome = document.querySelector('#nomeAluno').value;
+const curso = document.querySelector('#curso').value;
+const turno = document.querySelector('#turno').value ;
+
+
+const resultado = document.querySelector("#resultado");
+
+if(nome.trim() ==='' || curso === '' || turno ===''){
+    resultado.innerText = "Por favor, preencha todos os campos para continuar!"
+     resultado.style.color = "red"
+        return; 
+}
+
+resultado.innerHTML = `Olá, ${nome}, tudo bem?
+Sua pré-inscrição no curso de ${curso}, no turno da ${turno}, foi registrada com sucesso! Em breve, nossa equipe entrará em contato com você ${nome}, para dar continuidade ao processo seletivo`;
+resultado.style.color = "green";
+};
